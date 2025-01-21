b6f20bb44c4422a155149e0f33e535ce8e6450c3
quangdat <so2so3vaso5@gmail.com>
2025-01-21 23:00:03 +0700

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('index');
 });
-
+//Sáng
 Route::resource('users', UserController::class);
 Route::get('users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('users', [UserController::class, 'store'])->name('users.store');
@@ -29,15 +29,7 @@ Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edi
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 
 
-//quang dat
-Route::get('/', function () {
-    return view('index');
-});
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('coupons', CouponController::class);
-});
-
-
+//Đạt
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('coupons', CouponController::class);
 });
