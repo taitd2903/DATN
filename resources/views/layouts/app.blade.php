80ac39a367ae2f4c85bc56f94bd04dae7fd669db
wuyanh24 <quynhanh4008@gmail.com>
2025-03-14 01:51:25 +0700

    quynhanh update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,12 +59,12 @@
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                    <a class="navbar-brand" href="index.html"><img src="../assets/img/logo.png" class="logo" alt=""></a>
+                    <a class="navbar-brand" href="index.html"><img style="margin-left: 50px;width: 250px; height: 90px" src="../assets/img/logo.png" class="logo" alt=""></a>
                 </div>
                 <!-- End Header Navigation -->
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="navbar-menu">
+                <div class="collapse navbar-collapse" id="navbar-menu" style="margin-left: 400px;">
                     <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
                         <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
