0473420cb9ba2af81aa8e2676a1ff7c2651d67a8
wuyanh24 <quynhanh4008@gmail.com>
2025-03-14 19:41:41 +0700

    quynhanh

--- resources/views/Users/Products/index.blade.php
+++ resources/views/Users/Products/index.blade.php
@@ -2,36 +2,6 @@
 @extends('layouts.app')
 @section('content')
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
- </head>
- <body>
-    
-<!-- Favicons -->
-<link rel="icon" href="{{ asset('assets/img/favicon.png') }}">
-    <link rel="apple-touch-icon" href="{{ asset('assets/img/apple-touch-icon.png') }}">
-
-    <!-- Bootstrap 5 CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-
-    <!-- Vendor CSS Files -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/aos/aos.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
     <!-- Bộ lọc sản phẩm -->
     <!-- <form method="GET" action="{{ route('products.index') }}" class="mb-4">
         <div class="row g-3 align-items-center">
@@ -109,55 +79,53 @@
   
     <!-- SAN PHAM -->
     <div class="products-box">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="title-all text-center">
-                        <h1>Sản phẩm của chúng tôi</h1>
-                    </div>
-                </div>
-            </div>
-    <div class="row special-list">
-        @foreach($products as $product)
-        <div class="col-lg-3 col-md-6 special-grid best-seller">
-            <div class="products-single fix">
-                <div class="box-img-hover">
-                    <div class="type-lb">
-                        <p class="sale">Sale</p>
-                    </div>
-                    @if($product->image)
-                        <img src="{{ asset('storage/' . $product->image) }}" class="img-fluid" alt="{{ $product->name }}">
-                    @else
-                        <div class="p-3 text-center text-muted">Chưa có hình ảnh</div>
-                    @endif
-                    <div class="mask-icon">
-                        <ul>
-                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
-                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
-                            <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
-                        </ul>
-                        <a href="{{ route('products.show', $product->id) }}" class="cart">Xem chi tiết</a>
-                    </div>
-                </div>
-                <div class="why-text">
-                    <h4>{{ $product->name }}</h4>
-                    
-                    @php
-                        $minPrice = $product->variants->min('price') ?? 0;
-                        $maxPrice = $product->variants->max('price') ?? 0;
-                    @endphp
-    
-                    <h5 class="text-danger fw-bold">
-                        {{ number_format($minPrice, 0, ',', '.') }} VND - {{ number_format($maxPrice, 0, ',', '.') }} VND
-                    </h5>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="title-all text-center">
+                    <h1>Sản phẩm của chúng tôi</h1>
                 </div>
             </div>
         </div>
-    @endforeach
-    
-</div>
- 
-</div>
+        <div class="row special-list">
+            @foreach($products as $product)
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12 special-grid best-seller">
+                <div class="products-single fix">
+                    <div class="box-img-hover">
+                        <div class="type-lb">
+                            <p class="sale">Sale</p>
+                        </div>
+                        @if($product->image)
+                            <img src="{{ asset('storage/' . $product->image) }}" class="img-fluid" alt="{{ $product->name }}">
+                        @else
+                            <div class="p-3 text-center text-muted">Chưa có hình ảnh</div>
+                        @endif
+                        <div class="mask-icon">
+                            <ul>
+                                <li><a href="#" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
+                                <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
+                                <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
+                            </ul>
+                            <a href="{{ route('products.show', $product->id) }}" class="cart">Xem chi tiết</a>
+                        </div>
+                    </div>
+                    <div class="why-text">
+                        <h4>{{ $product->name }}</h4>
+                        
+                        @php
+                            $minPrice = $product->variants->min('price') ?? 0;
+                            $maxPrice = $product->variants->max('price') ?? 0;
+                        @endphp
+        
+                        <h5 style="font-size: 12px;">
+                            {{ number_format($minPrice, 0, ',', '.') }} VND - {{ number_format($maxPrice, 0, ',', '.') }} VND
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 
 <!-- BST -->
@@ -340,7 +308,7 @@
     
 
 
-
+<!-- 
 <style>
     .product-img {
         height: 200px;
@@ -369,9 +337,6 @@
     .col {
         display: flex;
     }
-</style>
+</style> -->
 
-</body>
-
- </html>
  @endsection
