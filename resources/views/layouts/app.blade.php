4b2c0e743d6e3fa28b8ded6c4214eff1cefc2977
thangcustion24 <nguyenquangthang241104@gmail.com>
2025-04-13 22:10:09 +0700

    layout trangc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -281,7 +281,7 @@
 <script src="{{ asset('assets/js/jquery.slicknav.js') }}"></script>
 <script src="{{ asset('assets/js/mixitup.min.js') }}"></script>
 <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
-<!-- <script src="{{ asset('assets/js/main.js') }}"></script> -->
+ <script src="{{ asset('assets/js/main.js') }}"></script> 
 
 <script>
     $(document).ready(function () {
